01a3c648764b0b20525cd1fd061319a2e686e0f0
Fateme Farokhzad <fatemefarokhzad98@gmail.com>
2024-01-29 12:16:45 +0330

    add migration and update database

--- src/ProductManagementApplication.EntityFrameworkCore/Migrations/20240129084157_xxxx.Designer.cs
+++ src/ProductManagementApplication.EntityFrameworkCore/Migrations/20240129084157_xxxx.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductManagementApplication.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ProductManagementApplication.Migrations
 {
     [DbContext(typeof(ProductManagementApplicationDbContext))]
-    partial class ProductManagementApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240129084157_xxxx")]
+    partial class xxxx
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
