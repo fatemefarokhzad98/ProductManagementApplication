eeefd2cade33df3eadc4c9e4dbe9311df1d9ec08
Fateme Farokhzad <fatemefarokhzad98@gmail.com>
2024-01-30 11:28:37 +0330

    add app srvice and  implements

---
+++ src/ProductManagementApplication.Application.Contracts/Products/IProductAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace ProductManagementApplication.Products
+{
+    public interface IProductAppService: IApplicationService
+    {
+        Task<PagedResultDto<ProductDto>> GetListAsync(PagedAndSortedResultRequestDto input);
+
+
+    }
+}
